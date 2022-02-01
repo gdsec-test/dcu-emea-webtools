60fb9e9cc87f291a863de19f991bcd8f24f43747
Wolfgang Stürmer <wstuermer@godaddy.com>
2022-02-01 15:48:52 +0100

    syntax error

--- urlcheck/index.php
+++ urlcheck/index.php
@@ -29,7 +29,7 @@
             $data = trim($data);
             $data = stripslashes($data);
             $data = htmlspecialchars($data);
-            $data = str_replace('hxxp', 'http' $data);
+            $data = str_replace("hxxp", "http", $data);
             $data = str_replace(']', , $data);
             $data = str_replace('[', , $data);
             return $data;
