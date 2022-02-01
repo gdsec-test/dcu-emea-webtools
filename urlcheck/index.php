c98ba6f2bea6ff40250ae9164baba35723b36672
Wolfgang Stürmer <wstuermer@godaddy.com>
2022-02-01 15:39:26 +0100

    enable error reporting for debug

--- urlcheck/index.php
+++ urlcheck/index.php
@@ -1,3 +1,8 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+?>
 <html>
     <head>
         <link rel="stylesheet" href="../common/ig_main.css" type="text/css">
