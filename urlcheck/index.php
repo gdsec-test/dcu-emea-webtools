fe65f6fca5b0bdc3953a922fc8a850e32d779585
Wolfgang Stürmer <wstuermer@godaddy.com>
2022-02-01 15:29:57 +0100

    De-Sanitize and infos for URL

---
+++ urlcheck/index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="../common/ig_main.css" type="text/css">
+    </head>
+    <body>
+        <form action="index.php" method="post">
+            <p>URL: <input type="textarea" name="url" value="<?php echo $result ?>" /></p>
+            <p><input type="submit" /></p>
+        </form>
+    <?php
+        //INPUT
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $result = sanitize($_POST["url"]);
+            $host = parse_url($result, PHP_URL_HOST));
+            $ip = gethostbyname($host);
+            $ch = curl_init($result);
+            curl_exec($ch);
+            $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            echo $host . "is hosted at ". $ip " - Return code for the provided URL: ". $http_code;
+        }
+
+        function sanitize($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            $data = str_replace(['hxxp','hXXp','[',']'], ['http', 'http'], $data);
+            return $data;
+        }
+    ?>
+    </body>
+</html>
