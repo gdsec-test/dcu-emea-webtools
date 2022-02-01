70e56ce9aec86daa92161c55f1edc1b3dd64fe5c
Wolfgang Stürmer <wstuermer@godaddy.com>
2022-02-01 15:36:32 +0100

    adjustments

--- urlcheck/index.php
+++ urlcheck/index.php
@@ -4,18 +4,19 @@
     </head>
     <body>
         <form action="index.php" method="post">
-            <p>URL: <input type="textarea" name="url" value="<?php echo $result ?>" /></p>
+            <p>URL: <input type="textarea" name="url" /></p>
             <p><input type="submit" /></p>
         </form>
     <?php
         //INPUT
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $result = sanitize($_POST["url"]);
             $host = parse_url($result, PHP_URL_HOST));
             $ip = gethostbyname($host);
             $ch = curl_init($result);
             curl_exec($ch);
             $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            echo "Analysis for: ". $result ."<br />";
             echo $host . "is hosted at ". $ip " - Return code for the provided URL: ". $http_code;
         }
 
