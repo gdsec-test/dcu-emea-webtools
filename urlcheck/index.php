eed90d4940fb88bd6461f6fea390fed669ea124e
Wolfgang Stürmer <wstuermer@godaddy.com>
2022-02-14 12:41:06 +0100

    (HEGAT-762) add validations and geoip features
    - check for valid urls to minimize curl-calls
    - check for AS number, if function is present
    - get abuse contact for ip address

--- urlcheck/index.php
+++ urlcheck/index.php
@@ -10,22 +10,32 @@
             <p><input type="submit" /></p>
         </form>
     <?php
-        //INPUT
         if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $result = sanitize((string)$_POST["url"]); // cast $_POST to string, to avoid an array injection
             $arr = explode("\r\n", $result);
             for ($i = 0; $i < count($arr); $i++) {
-                $host = parse_url($arr[$i], PHP_URL_HOST);
-                $ip = gethostbyname($host);
-                $ch = curl_init($arr[$i]);
-                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,10);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_exec($ch);
-                $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                echo "Analysis for: ". $arr[$i] ."<br />";
-                echo "<b>".$host . "</b> is hosted at <b>". $ip ."</b><br />";
-                echo "Return code for the provided URL: <b>". $http_code ."</b><br /><br />";
+                if (filter_var($arr[$i], FILTER_VALIDATE_URL)) {
+                    $host = parse_url($arr[$i], PHP_URL_HOST);
+                    $ip = gethostbyname($host);
+                    if (function_exists("geoip_asnum_by_name")) {
+                        $as = geoip_asnum_by_name($ip);
+                    } else {
+                        $as = '';
+                    }
+                    $oct = explode(".", $ip);
+			        $revarr = array($oct[3], $oct[2], $oct[1], $oct[0]);
+			        $revip = implode(".", $revarr);
+			        $abusec = dns_get_record("$revip.abuse-contacts.abusix.zone.", DNS_TXT);
+	                $ch = curl_init($arr[$i]);
+        	        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,10);
+                	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        	        curl_exec($ch);
+                	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	                echo "Analysis for: ". $arr[$i] ."<br />";
+        	        echo "<b>".$host . "</b> is hosted at <b>". $ip ."</b> => (". $as . ") => (" . $abusec[0]['entries'][0] . ")<br />";
+                    echo "Return code for the provided URL: <b>". $http_code ."</b><br /><br />";
+                }
             }
         }
 
@@ -39,3 +49,4 @@
     ?>
     </body>
 </html>
+
