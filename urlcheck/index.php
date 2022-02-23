9153caa5ac1fdc1a93a3aa89ccc0a238322f4371
Wolfgang Stürmer <wstuermer@godaddy.com>
2022-02-23 09:36:08 +0100

    (HEGAT-779) rework domain validation
    - if input does not contain protocol (e.g.: www.domain.com), add it to the beginning
    - suppress output, if nothing to print (print error message)

--- urlcheck/index.php
+++ urlcheck/index.php
@@ -1,9 +1,20 @@
+<?php 
+    $version = "1.4"
+    /* URL-Checker 
+    DO NOT any manual changes to this code. It will be vanished with next deployment.
+    Codebase: https://github.com/gdcorp-infosec/dcu-emea-webtools
+    Purpose: Accepts one or more URLs or domains in various sanitized forms,
+        returns the A-Record for the domain part,
+        AS number for the ip address (based on geo-ip),
+        abuse contact for ip (based on abusix),
+        http status code for given URL */
+?>
 <html>
     <head>
         <link rel="stylesheet" href="../common/ig_main.css" type="text/css">
     </head>
     <body>
-        <h3>URL De-Sanitizer</h3>
+        <h3>URL Checker <?php echo $version ?></h3>
         <form action="index.php" method="post">
             <label for="url">URL</label>
             <textarea id="url" name="url" rows="10" cols="50"></textarea></p>
@@ -14,26 +25,32 @@
             $result = sanitize((string)$_POST["url"]); // cast $_POST to string, to avoid an array injection
             $arr = explode("\r\n", $result);
             for ($i = 0; $i < count($arr); $i++) {
-                if (filter_var($arr[$i], FILTER_VALIDATE_URL)) {
-                    $host = parse_url($arr[$i], PHP_URL_HOST);
-                    $ip = gethostbyname($host);
-                    if (function_exists("geoip_asnum_by_name")) {
-                        $as = geoip_asnum_by_name($ip);
-                    } else {
-                        $as = '';
-                    }
-                    $oct = explode(".", $ip);
-			        $revarr = array($oct[3], $oct[2], $oct[1], $oct[0]);
-			        $revip = implode(".", $revarr);
-			        $abusec = dns_get_record("$revip.abuse-contacts.abusix.zone.", DNS_TXT);
-	                $ch = curl_init($arr[$i]);
-        	        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,10);
-                	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        	        curl_exec($ch);
-                	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	                echo "Analysis for: ". $arr[$i] ."<br />";
-        	        echo "<b>".$host . "</b> is hosted at <b>". $ip ."</b> => (". $as . ") => (" . $abusec[0]['entries'][0] . ")<br />";
+                $host = parse_url($arr[$i], PHP_URL_HOST);
+                if (!is_string($host)) {
+                    $scheme = "http://";
+                    $scheme .= $arr[$i];
+                    $host = parse_url($scheme, PHP_URL_HOST);
+                }
+                $ip = gethostbyname($host);
+                if (function_exists("geoip_asnum_by_name")) {
+                    $as = geoip_asnum_by_name($ip);
+                } else {
+                    $as = '';
+                }
+                $oct = explode(".", $ip);
+                $revarr = array($oct[3], $oct[2], $oct[1], $oct[0]);
+                $revip = implode(".", $revarr);
+                $abusec = dns_get_record("$revip.abuse-contacts.abusix.zone.", DNS_TXT);
+                $ch = curl_init($arr[$i]);
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,10);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_exec($ch);
+                $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                curl_close($ch);
+                if (is_string($host)) {
+                    echo "Analysis for: ". $arr[$i] ."<br />";
+                    echo "<b>".$host . "</b> is hosted at <b>". $ip ."</b> => (". $as . ") => (" . $abusec[0]['entries'][0] . ")<br />";
                     echo "Return code for the provided URL: <b>". $http_code ."</b><br /><br />";
                 } else {
                     echo $arr[$i] . "has been skipped, as this seems not to be a valid URL!";
